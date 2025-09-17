b2da044de25cb9f47d17647bf196b3554f7c21ef
Gang-Ryung Uh <gangryunguh@gmail.com>
2025-09-17 14:34:56 -0500

    ex7-add-local-array stack frame size adjustment for the compiler generated temporaries

--- add-local-array.s
+++ add-local-array.s
@@ -47,36 +47,47 @@ $L1:
 
     .globl main
 main:
+    # M[$29+52] is for $31
+    # M[$29+48] is for the compiler generated temporary variable k
+    # M[$29+44] is for the compiler generated temporary variable i
+    # M[$29+40] is for sum
+    # M[$29+0] is to larray[0]
 
     # function prolog
-    addiu $sp,$sp,-48
-    sw    $31,44($sp)
+    addiu $sp,$sp,-56
+    sw    $31,52($sp)  # for $31
 
-    # M[$29+4] is to larray[0]
-    # M[$29]   is for sum
 
-    # we need
     # for (i = 0, k=1; i < 10; i++,k++)
     #     larray[i] = k;
-    li $8,0
-    li $9,1
-    addiu $10,$sp,4  # &(larray[0])
+    sw $zero,44($sp) # i = 0
+    li $8,1
+    sw $8,48($sp)    # k = 1
+
 $L4:
-    slti $11,$8,10
-    beq $11,$0,$L3
+    lw $8,44($sp)
+    slti $9,$8,10
+    beq $9,$0,$L3
 
-    # larray[i] = k
-    # larray[i] = &(larray[0])+ i*4
-    sll $12,$8,2
-    addu $12,$10,$12
-    sw $9,0($12)
+    # larray[i] = i, where
+    #      larray[i] = &(larray[0])+ i*4
+    lw $8,44($sp)
+    sll $8,$8,2
+    addu $8,$sp,$8
+    lw $9,48($sp)
+    sw $9,0($8)
 
+    lw $8,44($sp)
     addiu $8,$8,1
-    addiu $9,$9,1
+    sw $8,44($sp)
+
+    lw $8,48($sp)
+    addiu $8,$8,1
+    sw $8,48($sp)
     b $L4
 
 $L3:
-    move     $a0,$10
+    move     $a0,$sp
     li     $a1,10
     jal    arraysum
 
@@ -96,6 +107,6 @@ $L3:
     syscall
 
     # epilog
-    lw    $31,44($sp)
-    addiu $sp,$sp,48
+    lw    $31,52($sp)
+    addiu $sp,$sp,56
     jr    $ra
--- ex7-add-local-array.c
+++ ex7-add-local-array.c
@@ -14,6 +14,11 @@ int arraysum(int arr[], int length) {
 }
 
 int main() {
+   // compiler needs to insert code that initialize
+   // the larray[] with
+   // int i,k;
+   // for (i=0,k=1; i < 10; i++,k++)
+   //      larray[i] = k;
    int larray[] = {1,2,3,4,5,6,7,8,9,10};
    int sum = arraysum(larray,10);
    printf("sum = %d\n", sum);
